78c3917bba49e5e75c6490ff88fa28f0dd51117b
rebeccaroos <rebecca_roos@berkeley.edu>
2023-03-16 21:16:38 -0700

    Add function to call API to get/display pollution data

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,8 @@
 const searchBtn = document.getElementById('search-btn');
 // retrieve the input value that has been entered into the search field
 const searchInputVal = document.getElementById('search-input').value;
+// container to put pollution data into
+const pollutionEl = document.getElementById('pollution-container');
 
 // when the "search" button is clicked, call the function to obtain the latitude and longitude values for the selected city.
 searchBtn.getEventListener('click', function(event) {
@@ -16,3 +18,24 @@ searchBtn.getEventListener('click', function(event) {
             getPollution(lat, lon);
         })
 })
+
+// Call API to get pollution data and display it on the page
+function getPollution(lat, lon) {
+    const requestUrl = `http://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=64df37f68b0627d21253529450289fdb`;
+
+    fetch(requestUrl)
+        .then(response => response.json())
+        .then(data => {
+            pollutionEl.innerHTML = `
+                <p>Air Quality Index (AQI): ${data.list[0].main.aqi}</p>
+                <p>Concentration of CO (carbon monoxide): ${data.list[0].components.co}</p>
+                <p>Concentration of NO (nitrogen monoxide): ${data.list[0].components.no}</p>
+                <p>Concentration of NO<sub>2</sub> (nitrogen dioxide): ${data.list[0].components.no2}</p>
+                <p>Concentration of O<sub>3</sub> (ozone): ${data.list[0].components.o3}</p>
+                <p>Concentration of SO<sub>2</sub> (sulphur dioxide): ${data.list[0].components.so2}</p>
+                <p>Concentration of PM<sub>2.5</sub> (fine particles matter): ${data.list[0].components.pm2_5}</p>
+                <p>Concentration of PM<sub>10</sub> (coarse particulate matter): ${data.list[0].components.pm10}</p>
+                <p>Concentration of NH<sub>3</sub> (ammonia): ${data.list[0].components.nh3}</p>
+                `
+        });
+}
